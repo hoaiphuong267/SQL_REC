c6b65f8033d772d7b35555424d7d61ee9685b1aa
Chaos <chaosking620@gmail.com>
2018-03-16 11:20:19 +0700

    thêm form interview creation (no code)

---
+++ Tuyendung/Tuyendung/Interview Creation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tuyendung
+{
+    public partial class Interview_Creation : Form
+    {
+        public Interview_Creation()
+        {
+            InitializeComponent();
+        }
+    }
+}
